c469be9ab755a5b3e3d9e3385cfb5e71df5914ae
Hendrik Sollich <hendrik@hoodie.de>
2018-10-28 18:35:31 +0100

    less minimal, more examples

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 async function main() {
     const js = await import("./pkg");
 
-    js.greet("Hendrik!");
+    js.greet("Team!");
+
+    js.draw();
+
+    js.call_me_maybe(message => { console.info(`OMG, they called me back with ${message}`)});
+    js.listen_for_keys();
 }
 
 main();
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,72 @@
+extern crate js_sys;
 extern crate wasm_bindgen;
+extern crate web_sys;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{Document, EventTarget, KeyboardEvent};
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(msg: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn debug(msg: &str);
 }
+
+#[used]
+static MAGIC: u32 = 0x42424242;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello {}, you're awsm!", name));
+    debug(&format!("Hello {}, you're awsm!", name));
+}
+
+fn get_document() -> Document {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+
+    document
+}
+
+#[wasm_bindgen]
+pub fn draw() -> Result<(), JsValue> {
+    debug("start drawing");
+
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let new_canvas = document
+        .create_element("canvas")
+        .expect(r"unable to create canvas from WASM /0\");
+
+    AsRef::<web_sys::Node>::as_ref(&body).append_child(new_canvas.as_ref())?;
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn call_me_maybe(cb: &js_sys::Function) -> Result<(), JsValue> {
+    let message = "I ❤️ you";
+
+    cb.call1(&JsValue::NULL, &JsValue::from(message))?;
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn listen_for_keys() -> Result<(), JsValue> {
+    let document = get_document();
+
+    let cb = Closure::wrap(Box::new(move |v: KeyboardEvent| {
+        debug(&format!("down wityh all the keys: {:#?}", v.key()))
+    }) as Box<Fn(_)>);
+
+    let et: &EventTarget = document.as_ref();
+    et.add_event_listener_with_callback("keydown", cb.as_ref().unchecked_ref())?;
+    cb.forget();
+
+    Ok(())
 }
